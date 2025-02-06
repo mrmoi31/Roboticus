fea3a40a68baa8bd937f2b6067d93fdc049b9cbb
mrmoi31 <dodo.leva.dl@gmail.com>
2025-02-06 16:52:30 +0100

    refacto code

--- roboticus.cpp
+++ roboticus.cpp
@@ -6,8 +6,8 @@
 
 #include "termCtrl.hpp"
 
-const int largeur = 7;
-const int longueur = 7;
+const int x = 7;
+const int y = 7;
 
 //struct Surdoue {
 //	int vie = 4;
@@ -27,8 +27,9 @@ struct Roboticus {
 	int vie = 0;
 	int dgt = 0;
 	int cout = 0;
-	int joueur = 0;
+	int joueur = -1;
 	std::string type = "          ";
+	bool moved = false;
 };
 
 struct Joueur {
@@ -37,42 +38,51 @@ struct Joueur {
 	int num = 0;
 };
 
-Roboticus board[largeur][longueur];
+Roboticus board[x][y];
 Joueur joueur1;
 Joueur joueur2;
 
 void display() {
-	std::cout<<"///|      A       |      B       |      C       |       D      |       E      |       F      |       G      |"<<std::endl;
-	std::cout<<"---+--------------+--------------+--------------+--------------+--------------+--------------+--------------+"<<std::endl;
-	std::cout<<" 0 | -"+board[0][0].type+"- | -"+board[0][1].type+"- | -"+board[0][2].type+"- | -"+board[0][3].type+"- | -"+board[0][4].type+"- | -"+board[0][5].type+"- | -"+board[0][6].type+"- |"<<std::endl;
-	std::cout<<"---+--------------+--------------+--------------+--------------+--------------+--------------+--------------+"<<std::endl;
-	std::cout<<" 1 | -"+board[1][0].type+"- | -"+board[1][1].type+"- | -"+board[1][2].type+"- | -"+board[1][3].type+"- | -"+board[1][4].type+"- | -"+board[1][5].type+"- | -"+board[1][6].type+"- |"<<std::endl;
-	std::cout<<"---+--------------+--------------+--------------+--------------+--------------+--------------+--------------+"<<std::endl;
-	std::cout<<" 2 | -"+board[2][0].type+"- | -"+board[2][1].type+"- | -"+board[2][2].type+"- | -"+board[2][3].type+"- | -"+board[2][4].type+"- | -"+board[2][5].type+"- | -"+board[2][6].type+"- |"<<std::endl;
-	std::cout<<"---+--------------+--------------+--------------+--------------+--------------+--------------+--------------+"<<std::endl;
-	std::cout<<" 3 | -"+board[3][0].type+"- | -"+board[3][1].type+"- | -"+board[3][2].type+"- | -"+board[3][3].type+"- | -"+board[3][4].type+"- | -"+board[3][5].type+"- | -"+board[3][6].type+"- |"<<std::endl;
-	std::cout<<"---+--------------+--------------+--------------+--------------+--------------+--------------+--------------+"<<std::endl;
-	std::cout<<" 4 | -"+board[4][0].type+"- | -"+board[4][1].type+"- | -"+board[4][2].type+"- | -"+board[4][3].type+"- | -"+board[4][4].type+"- | -"+board[4][5].type+"- | -"+board[4][6].type+"- |"<<std::endl;
-	std::cout<<"---+--------------+--------------+--------------+--------------+--------------+--------------+--------------+"<<std::endl;
-	std::cout<<" 5 | -"+board[5][0].type+"- | -"+board[5][1].type+"- | -"+board[5][2].type+"- | -"+board[5][3].type+"- | -"+board[5][4].type+"- | -"+board[5][5].type+"- | -"+board[5][6].type+"- |"<<std::endl;
-	std::cout<<"---+--------------+--------------+--------------+--------------+--------------+--------------+--------------+"<<std::endl;
-	std::cout<<" 6 | -"+board[6][0].type+"- | -"+board[6][1].type+"- | -"+board[6][2].type+"- | -"+board[6][3].type+"- | -"+board[6][4].type+"- | -"+board[6][5].type+"- | -"+board[6][6].type+"- |"<<std::endl;
-	std::cout<<"---+--------------+--------------+--------------+--------------+--------------+--------------+--------------+"<<std::endl;
+	//*****************
+	// display colonnes
+	//*****************
+	std::cout << "////|";
+	for (int i = 0; i < x; ++i)	{
+		std::cout << "      " << (char)(i + 65) << "       |";
+	}
+	std::cout<<"\n";
+	//*****************
+	// display lignes
+	//*****************
+	for (int i = 0; i < x; ++i)	{
+		std::cout << "----+";
+		for (int i = 0; i < x; ++i)	{
+			std::cout << "--------------+";
+		}
+		std::cout << "\n";
+		printf(" %02d |", i);
+		for (int j = 0; j < y; ++j) {
+			std::cout << " -" << board[i][j].type << "- |";
+		}
+		std::cout << "\n";
+	}
+	std::cout << "----+";
+	for (int i = 0; i < x; ++i)	{
+		std::cout << "--------------+";
+	}
+	std::cout << "\n";
 }
 
 int main() {
 	bool end = false;
-	int joueurs[2] = {1,2};
+	int joueurs[2] = {0,1};
 	joueur1.num = 1;
 	joueur2.num = 2;
-	//int joueur1 = 10;
-	//int joueur2 = 10;
 	std::string color[2] = {TC_RED, TC_BLU};
 	int round = 0;
 	int choix = 0;
 	char colonne = 'Z';
 	int col = 0;
-	//int points = 0;
 	display();
 
 	play:
@@ -80,9 +90,9 @@ int main() {
 	// Boucle de jeu
 	//***************************
 	while (!end) {
-		std::cout<< "Tour de joueur " << joueurs[round%2] <<std::endl;
-		std::cout<< "Phase de placement" << std::endl;
-		std::cout<<"-------------------"<< std::endl;
+		std::cout << "Tour de joueur " << joueurs[round%2] + 1 << std::endl;
+		std::cout << "Phase de placement" << std::endl;
+		std::cout << "-------------------" << std::endl;
 
 		do {
 			//***************************
@@ -119,7 +129,7 @@ int main() {
 		//***************************
 		col = int(colonne) - 65;
 		if (round%2 == 0) {
-			for (int i = longueur - 1; i >= 0; i--) {
+			for (int i = y - 1; i >= 0; --i) {
             	if (board[i][col].type == "          ") {
             		if (choix == 1) {
             			board[i][col].type = color[round%2] + "  Intello " + TC_RES;
@@ -127,6 +137,7 @@ int main() {
             			board[i][col].dgt = 1;
             			board[i][col].cout = 1;
             			board[i][col].joueur = joueurs[round%2];
+            			printf("piece à %d\n", board[i][col].joueur);
             		} else {
             			board[i][col].type = color[round%2] + "Combattant" + TC_RES;
             			board[i][col].vie = 2;
@@ -145,7 +156,7 @@ int main() {
 		//Gestion pose troupe j1
 		//***************************
 		else {
-			for (int i = 0; i <= longueur; i++) {
+			for (int i = 0; i <= y; ++i) {
             	if (board[i][col].type == "          ") {
             		if (choix == 1) {
             			board[i][col].type = color[round%2] + "  Intello " + TC_RES;
@@ -153,6 +164,7 @@ int main() {
             			board[i][col].dgt = 1;
             			board[i][col].cout = 1;
             			board[i][col].joueur = joueurs[round%2];
+            			printf("piece à %d\n", board[i][col].joueur);
             		} else {
             			board[i][col].type = color[round%2] + "Combattant" + TC_RES;
             			board[i][col].vie = 2;
@@ -161,7 +173,7 @@ int main() {
             			board[i][col].joueur = joueurs[round%2];
             		}
             	    break;
-            	} else if (i == longueur ) {
+            	} else if (i == y ) {
             	    std::cout << "La colonne est remplie\n";
             	    goto play;
             	}
@@ -170,105 +182,145 @@ int main() {
             display();
 
 		std::cout<< "Phase de combat" << std::endl;
-		std::cout<<"-------------------"<< std::endl;
-		//std::cout << color[round%2];
+		std::cout<< "-------------------" << std::endl;
 		
-		for (int i = 0; i < largeur; ++i){
-			for (int j = 0; j < longueur; ++j){
-				if (board[i][j].type != "          "){
+		for (int i = 0; i < x; ++i) {
+			for (int j = 0; j < y; ++j) {
+				if (board[i][j].type != "          " && board[i][j].joueur == 0) {
+
+					//***************************
+					//Gestion vie et degat j1
+					//***************************
+					if (i == 0 && board[i][j].joueur == 0) {
+						printf("dgt j1\n");
+						joueur2.vie -= board[i][j].dgt;
+						board[i][j].type = "          ";
+						board[i][j].dgt = 0;
+						board[i][j].joueur = -1;
+					}
 
 					//***************************
 					// Gestion combat j1
 					//***************************
-					if (board[i][j].dgt > board[i-1][j].vie){
-						board[i-1][j].type = "          ";
-						board[i-1][j].vie = 0;
-					} else if (board[i][j].dgt == board[i-1][j].vie){
-						board[i-1][j].type = "          ";
-						board[i-1][j].vie = 0;
+					if (board[i][j].dgt >= board[i - 1][j].vie) {
+						printf("fight j1\n");
+						board[i - 1][j].type = "          ";
+						board[i - 1][j].vie = 0;
+						board[i - 1][j].joueur = -1;
+					} else if (board[i - 1][j].dgt > board[i][j].vie) {
+						printf("defaite j1\n");
 						board[i][j].type = "          ";
 						board[i][j].vie = 0;
+						board[i][j].joueur = -1;
+						board[i][j].moved = true;
+					} else if (board[i][j].dgt == board[i - 1][j].vie && board[i - 1][j].dgt == board[i][j].vie) {
+						printf("equality j1\n");
+						board[i][j].type = "          ";
+						board[i][j].vie = 0;
+						board[i][j].joueur = -1;
+						board[i][j].moved = true;
+						board[i - 1][j].type = "          ";
+						board[i - 1][j].vie = 0;
+						board[i - 1][j].joueur = -1;
+						board[i - 1][j].moved = true;
 					}
 					
 					//***************************
 					//Gestion déplacement j1
 					//***************************
-					if (board[i][j].joueur == 1 && board[i-1][j].type == "          "){
-						board[i-1][j] = board[i][j];
+					if (board[i][j].joueur == 0 && board[i - 1][j].type == "          " && board[i][j].moved == false) {
+						printf("move j1\n");
+						board[i - 1][j] = board[i][j];
 						board[i][j].type = "          ";
+						board[i][j].joueur = -1;
+						board[i][j].moved = true;
 					}
 
+
+				} else if (board[i][j].type != "          " && board[i][j].joueur == 1) {
+
 					//***************************
-					//Gestion vie et degat j1
+					// Gestion dgt et vie j2
 					//***************************
-					if (i == 0 && board[i][j].joueur == 1){
-						joueur2.vie -= board[i][j].dgt;
+					if (i == y - 1 && board[i][j].joueur == 1 && board[i][j].moved == false) {
+						printf("dgt j2\n");
+						joueur1.vie -= board[i][j].dgt;
 						board[i][j].type = "          ";
 						board[i][j].dgt = 0;
+						board[i][j].joueur = -1;
+						board[i][j].moved = true;
 					}
 
-				}
-			}
-		}
-
-		for (int i = largeur - 1; i >= 0; --i){
-			for (int j = longueur - 1; j >= 0; --j){
-				
-				//***************************
-				// Gestion combat j2
-				//***************************
-				if (board[i][j].type != "          "){
-					if (board[i][j].dgt > board[i+1][j].vie){
-						board[i+1][j].type = "          ";
-						board[i+1][j].vie = 0;
-					} else if (board[i][j].dgt == board[i+1][j].vie){
-						board[i+1][j].type = "          ";
-						board[i+1][j].vie = 0;
+					//***************************
+					// Gestion combat j2
+					//***************************
+					if (board[i][j].dgt >= board[i + 1][j].vie && board[i][j].moved == false) {
+						printf("fight j2\n");
+						board[i + 1][j].type = "          ";
+						board[i + 1][j].vie = 0;
+						board[i + 1][j].joueur = -1;
+						board[i + 1][j].moved = true;
+					} else if (board[i + 1][j].dgt > board[i][j].vie) {
+						printf("defaite j2\n");
 						board[i][j].type = "          ";
 						board[i][j].vie = 0;
+						board[i][j].joueur = -1;
+						board[i][j].moved = true;
+					} else if (board[i][j].dgt == board[i + 1][j].vie && board[i + 1][j].dgt == board[i][j].vie) {
+						printf("equality j2\n");
+						board[i][j].type = "          ";
+						board[i][j].vie = 0;
+						board[i][j].joueur = -1;
+						board[i][j].moved = true;
+						board[i + 1][j].type = "          ";
+						board[i + 1][j].vie = 0;
+						board[i + 1][j].joueur = -1;
+						board[i + 1][j].moved = true;
 					}
 
 					//***************************
 					// Gestion deplacement j2
 					//***************************
-					if (board[i][j].joueur == 2 && board[i+1][j].type == "          "){
-						board[i+1][j] = board[i][j];
+					if (board[i][j].joueur == 1 && board[i + 1][j].type == "          " && board[i][j].moved == false) {
+						printf("move j2\n");
+						board[i + 1][j] = board[i][j];
+						board[i + 1][j].moved = true;
 						board[i][j].type = "          ";
-					}
-					
-					//***************************
-					// Gestion dgt et vie j2
-					//***************************
-					if (i == longueur - 1 && board[i][j].joueur == 2){
-						joueur1.vie -= board[i][j].dgt;
-						board[i][j].type = "          ";
-						board[i][j].dgt = 0;
+						board[i][j].joueur = -1;
 					}
 				}
 			}
 		}
 
-		std::cout << "joueur1 ";
+		std::cout << "joueur1 : ";
 		std::cout << joueur1.vie << std::endl;
-		std::cout << "joueur2 ";
+		std::cout << "joueur2 : ";
 		std::cout << joueur2.vie << std::endl;
 		std::cout << TC_RES;
-		
+
+		round++;
+
+		for (int i = 0; i < x; ++i) {
+			for (int j = 0; j < y; ++j) {
+				printf("cleared moved\n");
+				board[i][j].moved = false;
+			}
+		}
+
 		display();
 
-		//combat
-		//refaire display
+		//fix combat
+		//refacto
 		//si 2 à coté same type fusion
 
 		//***************************
 		//Gestion victoire
 		//***************************
-		if (joueur1.vie <= 0 || joueur2.vie <= 0){
+		if (joueur1.vie <= 0 || joueur2.vie <= 0) {
 			end = true;
 			break;
 		}
 
-		round++;
 	}
 
 	std::cout << "*************************************" << std::endl;
